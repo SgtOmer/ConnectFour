170b3c9bb3ea5af9ed9dd135ad981933680b6ca7
Omer Atmon <omeratz@gmail.com>
2016-07-29 00:26:11 +0300

    starting winner checking

--- ConnectFour/src/ConnectFour/Main.java
+++ ConnectFour/src/ConnectFour/Main.java
@@ -5,16 +5,84 @@ public class Main {
 	final static char nullOnBoard='0';
 	final static char xOnBoard='X';
 	final static char oOnBoard='O';
+	final static int cols=7;
+	final static int rows=6;
 	
 	/**
 	 * @param board the board of the game
 	 */
 	public static void reset(char[][] board){
-		for (int i=0 ; i<board.length ; i++){
+		for (int i=0 ; i<board.length-3 ; i++){
 			for (int j=0 ; j<board[i].length ; j++){
 				board[i][j] = nullOnBoard;
 			}
 		}
+	}
+	
+	public static char[] getCol(char[][] board, int index){
+		char[] col = new char[board.length];
+		for (int i=0 ; i<board.length ; i++)
+			col[i] = board[i][index];
+		return col;
+	}
+	
+	/**
+	 * @param board the board of the game
+	 * @return 1-X wined 2-O wined 0-no winner in a specific row
+	 */
+	public static int rowWinner(char[] array){
+		for (int i=0 ; i<array.length-3 ; i++){
+			if (array[i]==array[i+1]&&array[i+2]==array[i+3]&&array[i]==array[i+2]){
+				if (array[i]==xOnBoard)
+					return 1;
+				else if (array[i]==oOnBoard)
+					return 2;
+			}
+		}
+		return 0;
+	}
+	
+	public static int colWinner(char[] array){
+		for (int i=0 ; i<array.length-3 ; i++){
+			if (array[i]==array[i+1]&&array[i+2]==array[i+3]&&array[i]==array[i+2]){
+				if (array[i]==xOnBoard)
+					return 1;
+				else if (array[i]==oOnBoard)
+					return 2;
+			}
+		}
+		return 0;
+	}
+
+	public static int Winner(char[][] board){
+	
+		return 0;
+	}
+
+	public static int rowWnner(char[][] board){
+		
+		return 0;
+	}
+	
+	/**
+	 * @param board the board of the game
+	 * @return 1-X wined 2-O wined 0-no winner
+	 */
+	public static int winner(char[][] board){
+		int winner;
+		
+		for (int i=0 ; i<rows ; i++){
+			winner = rowWinner(board[i]);
+			if (winner!=0)
+				return winner;
+		}
+		
+		for (int i=0 ; i<cols ; i++){
+			winner = colWinner(getCol(board,i));
+			if (winner!=0)
+				return winner;
+		}
+		return 0;
 	}
 
 	public static void main(String[] args) {
--- ConnectFour/src/ConnectFour/mainTests.java
+++ ConnectFour/src/ConnectFour/mainTests.java
@@ -1,6 +1,8 @@
 package ConnectFour;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class mainTests {
@@ -8,6 +10,7 @@ public class mainTests {
 	final static char nullOnBoard='0';
 	final static char xOnBoard='X';
 	final static char oOnBoard='O';
+	char[][] array;
 
 	@Test
 	public void testReset() {
@@ -27,5 +30,32 @@ public class mainTests {
 				board[i][j] = nullOnBoard;
 			}
 		}
+	}
+	
+	@Before
+	public void setUp(){
+		array = new char[][]{
+			{'1','2','3','4','5'},
+			{'2','3','4','5','6'},
+			{'3','4','5','6','7'},
+			{'4','5','6','7','8'}
+		};
+	}
+	
+	@Test
+	public void gettingCol(){
+		int colNum=(int)(Math.random()*5);
+		char[] col = getCol(array,colNum);
+		for (int i=0 ; i<array.length ; i++){
+			if (col[i] != array[i][colNum])
+				fail();
+		}
+	}
+	
+	public static char[] getCol(char[][] board, int index){
+		char[] col = new char[board.length];
+		for (int i=0 ; i<board.length ; i++)
+			col[i] = board[i][index];
+		return col;
 	}
 }
